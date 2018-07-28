3979f6409f0f0dbd5bca0cf5eb4197b4d0125f5e
James sandford <sandford.james@gmail.com>
2018-07-28 16:06:41 +0900

    fixed a mistake

--- index.js
+++ index.js
@@ -4,81 +4,18 @@ var rp = require('request-promise');
 var connectConfig = require('./connectConfig.js');
 var ChatBotCom = require('./ChatBotCommands.js');
 var User = require('./user.js');
+//var users = []
+//var currentUsers = []
 var addr;
 var port;
 var streamState = false;
 var liveBeet;
 var pointbeet;
 console.log("start");
-User.load();
+load();
 console.log("file loaded");
 let commandPrefix = '!'
 
-//bot commands go here
-let knownCommands = {
-    //listAllUsers, 
-    //listCurrentUsers, 
-    getPoints,
-    givePoints,
-    echo,
-    saveFile,
-    help}
-
-function listAllUsers(target, context, params){
-    sendMessage(target, context, JSON.stringify(users));
-}
-function listCurrentUsers(target, context, params){
-    sendMessage(target, context, JSON.stringify(currentUsers));
-}
-
-function getPoints(target, context, params){
-        if(!containsUsername(users,params[0])){
-        sendMessage(target,context,`unknown user: ${params[0]}`)
-            console.log("unknown user: " + params[0]);
-    }else{
-        sendMessage(target,context,`${params[0]} has ${getUserUn(params[0]).points} points`)// "give " + params[0] + " points");
-    
-    }
-}
-function givePoints(target, context, params){
-    if(!containsUsername(users,params[0])){
-        sendMessage(target,context,`unknown user: ${params[0]}`)
-            console.log("unknown user: " + params[0]);
-    }else{
-        giveUserPoints(getUserUn(params[0]),params[1]);
-        //getUserUn(params[0]).points = parseInt(getUserUn(params[0]).points)+ parseInt(params[1]);
-        sendMessage(target,context,`given ${params[0]} ${params[1]} points`)// "give " + params[0] + " points");
-    
-    }
-}
-
-function saveFile(){
-    save();
-}
-function help(target, context, params){
-    
-}
-// Function called when the "echo" command is issued:
-function echo (target, context, params) {
-  // If there's something to echo:
-  if (params.length) {
-    // Join the params into a string:
-    const msg = params.join(' ')
-    // Send it back to the correct place:
-    sendMessage(target, context, msg)
-  } else { // Nothing to echo
-    console.log(`* Nothing to echo`)
-  }
-}
-
-function sendMessage(target,context,message){
-    if(context['message-type'] == 'whisper'){
-        client.whisper(target,message);
-    }else{
-        client.say(target,message);
-    }
-}
-*/
 function giveUserPoints(user, points){
     user.points = parseInt(user.points)+ parseInt(points)
 }
@@ -112,9 +49,8 @@ function onMessageHandler(target,context,msg,self){
     console.log(`[${target}(${context['message-type']})]${context.username}: ${msg}`);
     return;
     }
-    ChatBotCom.runCommand(target,context,msg, client);
+    ChatBotCom.runCommand(target,context,msg);
 
-}
 function onUserJoin(channel, username){
     console.log(username + ' joined');
     //save();
@@ -137,59 +73,7 @@ function noDissconnectedHandler(reason){
       console.log(`Womp womp, disconnected: ${reason}`)
     clearInterval(liveBeet);
 
-}
 
-function save(){
-    fs.writeFile("Users.json",JSON.stringify(users),"utf8",onSave);
-}
-function onSave(){
-}
-function load(){
-    users = require("./Users.json");
-    console.log(users);
-}
-function addCurrentUsers(username){
-    if(containsUsername(currentUsers,username))
-        return;
-    currentUsers.push(getUserUn(username));
-}
-function removeCurrentUser(username){
-    if(containsUsername(currentUsers,username))
-    currentUsers.pop(getUserUn(username));
-}
-function addUser(username){
-    if(containsUsername(users, username))
-        return ;
-    users.push(new user(username));
-    console.log(username + ' is a new user');
-    save();
-}
-function getUserUn(username){
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].username === username) {
-            return users[i];
-        }
-    }
-}
-function user(username){
-    this.username = username;
-    this.ID = GenID();
-    this.points = parseInt(0);
-}
-function GenID() {
-  return 'xxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
-function containsUsername(a, username) {
-    for (var i = 0; i < a.length; i++) {
-        if (a[i].username === username) {
-            return true;
-        }
-    }
-    return false;
-}*/
 function GetStreamingState(){
     rp(connectConfig.htmlOptions)
     .then(function(resp){
